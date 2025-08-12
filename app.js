8680664aaaf0f9ce7b6f825845d69a124eed9f79
Tobias German <tobigerman2019@gmail.com>
2025-08-12 11:55:35 -0300

    Update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Rutas de prueba
-app.get('/', (req, res) => res.send('Hola desde /'));
+app.get('/', (req, res) => res.send('Aieka /'));
 app.get('/about', (req, res) => res.send('Página About'));
 
 app.listen(port, () => console.log(`Servidor escuchando en puerto ${port}`));
